2ae8ec55e46ffed1c7e7995e6af3fe2a29edca84
Marco Trevisan <evenjn@users.noreply.github.com>
2017-04-25 17:01:52 +0200

    Improved documentation.

--- src/main/java/org/github/evenjn/yarn/package-info.java
+++ src/main/java/org/github/evenjn/yarn/package-info.java
@@ -1,6 +1,19 @@
 /**
+ * <p>
  * Package Yarn is a collection of interfaces that enable folding and unfolding
  * with state and resources.
+ * </p>
+ * 
+ * <h1>Purl</h1>
+ * <p>
+ * To purl a sequence of elements means to transform it into another sequence of
+ * elemenets by means of a purl. A purl is a system designed to receive input
+ * elements one by one, and for each input, to produce zero, one or more output
+ * elements. The purl may (and will) be stateful: it may accumulate information
+ * while processing elements, and use such information to produce output
+ * elements. Moreover, a purl is designed to be invoked one last time after it
+ * has received all the input elements. At that time the purl has a chance to
+ * produce more output elements.
+ * </p>
  */
 package org.github.evenjn.yarn;
-
