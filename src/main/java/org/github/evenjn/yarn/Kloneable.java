b6af3d12a7523ac38f7aea89b7c81382e7751eb0
Marco Trevisan <evenjn@users.noreply.github.com>
2017-08-19 14:05:00 +0200

    Added Kloneable.

---
+++ src/main/java/org/github/evenjn/yarn/Kloneable.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * Copyright 2017 Marco Trevisan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.github.evenjn.yarn;
+
+public interface Kloneable {
+
+	/**
+	 * Returns a copy of the argument object, which must be {@code this}.
+	 * 
+	 * @param <K>
+	 *          The type of the argument.
+	 * @param kloneable
+	 *          the object to copy
+	 * @return a copy of the input argument
+	 * @throws IllegalArgumentException
+	 *           the argument is not {@code this}.
+	 * @since 1.0
+	 */
+	<K extends Kloneable> K klone( K kloneable )
+			throws IllegalArgumentException;
+}
