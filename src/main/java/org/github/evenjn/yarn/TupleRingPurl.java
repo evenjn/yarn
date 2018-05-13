3841ad0bc5cfcaabef40c96f69ee187a9e118bb5
Marco Trevisan <evenjn@users.noreply.github.com>
2018-05-13 17:22:28 +0200

    Documentation.

--- src/main/java/org/github/evenjn/yarn/TupleRingPurl.java
+++ src/main/java/org/github/evenjn/yarn/TupleRingPurl.java
@@ -26,7 +26,7 @@ import org.github.evenjn.yarn.Tuple;
  * <p>
  * A {@code TupleRingPurl} is a {@link org.github.evenjn.yarn.YarnRingPurl
  * YarnRingPurl} that provides access to output objects via
- * {@link java.util.stream.Tuple Tuple} containers.
+ * {@link org.github.evenjn.yarn.Tuple Tuple} containers.
  * </p>
  * 
  * <p>
@@ -44,7 +44,7 @@ public interface TupleRingPurl<I, O> extends
 
 	/**
 	 * <p>
-	 * {@code next} returns a {@link java.util.stream.Tuple Tuple} with none of,
+	 * {@code next} returns a {@link org.github.evenjn.yarn.Tuple Tuple} with none of,
 	 * some of, or all the output objects associated to the sequence of elements
 	 * received in input so far (including the argument {@code input}), while
 	 * transferring the responsiblity of closing any associated resources to the
@@ -55,7 +55,7 @@ public interface TupleRingPurl<I, O> extends
 	 *          A {@link org.github.evenjn.lang.Rook Rook}.
 	 * @param input
 	 *          An input object.
-	 * @return A {@link java.util.stream.Tuple Tuple} of output objects.
+	 * @return A {@link org.github.evenjn.yarn.Tuple Tuple} of output objects.
 	 * @throws IllegalStateException
 	 *           when {@link #end(Rook)} has already been invoked.
 	 * @since 1.0
@@ -65,7 +65,7 @@ public interface TupleRingPurl<I, O> extends
 
 	/**
 	 * <p>
-	 * {@code end} returns a {@link java.util.stream.Tuple Tuple} with none of,
+	 * {@code end} returns a {@link org.github.evenjn.yarn.Tuple Tuple} with none of,
 	 * some of, or all the output objects associated to the sequence of elements
 	 * received in input so far, while transferring the responsiblity of closing
 	 * any associated resources to the argument {@link org.github.evenjn.lang.Rook
@@ -74,7 +74,7 @@ public interface TupleRingPurl<I, O> extends
 	 * 
 	 * @param rook
 	 *          A {@link org.github.evenjn.lang.Rook Rook}.
-	 * @return A {@link java.util.stream.Tuple Tuple} of output objects.
+	 * @return A {@link org.github.evenjn.yarn.Tuple Tuple} of output objects.
 	 * @throws IllegalStateException
 	 *           when {@link #end(Rook)} has already been invoked.
 	 * @since 1.0
